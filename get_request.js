bbf15e6a3e43bc4072a537e4a1268f332586e937
joshua <josh@joshuadennis.ca>
2019-01-15 01:46:00 +0000

    added comments and everything works, yay

--- get_request.js
+++ get_request.js
@@ -8,9 +8,13 @@ request.get('https://sytantris.github.io/http-examples')               // Note 1
        })
        .on('response', function (response) {                           // Note 3
          console.log('The Status is: ', response.statusCode, 'and the Content-type is:', response.headers['content-type']);
+         console.log('We be downloading your pic.');
+
+       })
+       .on('end', function() {
+         console.log('Your sweet jpg is done downloading, peace!');
        })
        .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
-    
 // Notes:
 // 1. `request.get` is equivalent to `request()`
 // 2. `request.on('error', callback)` handles any error
